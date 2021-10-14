a4c3670638342969b78619a1ebe70255a762bfdb
MARCIO LUIS HAMASAKI DA SILVA <marcio.hamasaki@sc.senai.br>
2021-10-14 07:53:28 -0300

    Added user custom model.

---
+++ app/core/models.py
@@ -0,0 +1,42 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
+                                        PermissionsMixin
+
+
+class UserManager(BaseUserManager):
+
+    def create_user(self, email, password=None, **extra_fields):
+        """Creates and saves a new user"""
+        if not email:
+            raise ValueError('Users must have an email address')
+        # SELF.MODEL access the model which is being managed.
+        # (cont) This is setup by BaseUserManager.
+        user = self.model(email=self.normalize_email(email), **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, password):
+        """Creates and saves a new super user"""
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """Custom user model that supports using email instead of username"""
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    # We use UserManager() to manage objects for the User Model.
+    # (cont) It's accessed by User.objects, so that the manager
+    # (cont) can be aware of the model it is managing.
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
---
+++ app/core/tests/test_models.py
@@ -0,0 +1,47 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+
+class ModelTests(TestCase):
+
+    def test_create_user_with_email_successful(self):
+        """Test creating a new user with an email is successful"""
+        email = 'test@test.com'
+        password = 'TestPassword123'
+
+        # get_user_model() is a helper function provided by Django
+        # (cont) which returns the user model set in AUTH_USER_MODEL
+        # (cont) defined in settings.py
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password
+        )
+
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        """Test the email for a new user is normalized"""
+        email = 'test@TEST.COM'
+        user = get_user_model().objects.create_user(email, 'test123')
+
+        self.assertEqual(user.email, email.lower())
+
+    def test_new_user_invalid_email(self):
+        """Test creating user with no email raises error"""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(None, 'test123')
+
+    def test_create_new_superuser(self):
+        """Test creating a new superuser"""
+        email = 'test@test123.com'
+        password = 'test123'
+
+        user = get_user_model().objects.create_superuser(
+            email=email,
+            password=password
+        )
+
+        self.assertTrue(user.is_staff)
+        # is_superuser é uma flag incluída em PermissionsMixin
+        self.assertTrue(user.is_superuser)
